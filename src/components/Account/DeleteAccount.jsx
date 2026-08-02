d329ff93d6f5a675f6362df833ca2991131fa237
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-08-03 01:09:12 +0500

    feat: modify account settings layout for dark theme

--- src/components/Account/DeleteAccount.jsx
+++ src/components/Account/DeleteAccount.jsx
@@ -4,16 +4,17 @@ import { TriangleAlert } from 'lucide-react';
 
 function DeleteAccount() {
     return (
-        <div className='rounded-lg border shadow-lg text-zinc-900 bg-white p-6 mt-5'>
-            <h1 className='text-3xl font-bold text-zinc-900'>
+        <div className='rounded-lg shadow-lg text-zinc-900 bg-white p-6 mt-5 duration-500
+        dark:bg-zinc-700'>
+            <h1 className='text-3xl font-bold text-zinc-900 duration-500 dark:text-white'>
                 Delete Account
                 <span className='text-red-500 inline-block ml-2'><TriangleAlert size={20}/></span>
             </h1>
-            <p className='text-sm mt-1'>
+            <p className='text-sm mt-1 duration-500 dark:text-white'>
                 Permanently delete your account and all associated data. This action cannot be undone.
             </p>
             <Button bgColor='bg-red-700' textColor='text-white'
-                className='text-sm mt-4 hover:bg-red-500 duration-200'
+                className='text-sm mt-4 hover:bg-red-500 duration-200 font-bold'
             >
                 Delete My Account
             </Button>
--- src/components/Account/UpdateEmail.jsx
+++ src/components/Account/UpdateEmail.jsx
@@ -3,20 +3,24 @@ import { Input, Button } from '../index.js'
 
 function UpdateEmail() {
     return (
-        <div className='rounded-lg border shadow-lg text-zinc-900 bg-white p-6 mt-5'>
-            <h1 className='text-3xl font-bold text-zinc-900'>
+        <div className='rounded-lg shadow-lg text-zinc-900 bg-white p-6 mt-5 duration-500
+        dark:bg-zinc-700'>
+            <h1 className='text-3xl font-bold text-zinc-900 duration-500 dark:text-white'>
                 Update Email
             </h1>
-            <p className='text-sm mt-1'>
+            <p className='text-sm mt-1 duration-500 dark:text-white'>
                 Enter your new email address below to update your account.
             </p>
             <div className='w-1/4'>
                 <Input type='email' placeholder='New email'
-                    className='mt-4 border-2 focus:border-black'
+                    className='mt-4 border-2 focus:border-black duration-500
+                    dark:bg-zinc-700 dark:border-zinc-600 dark:focus:bg-zinc-700 
+                    dark:text-white dark:focus:border-zinc-800'
                 />
             </div>
             <Button bgColor='bg-slate-900' textColor='text-white'
-                className='text-sm mt-4 hover:bg-slate-700 duration-200'
+                className='text-sm mt-4 hover:bg-slate-700 duration-200 font-bold 
+                dark:bg-lime-500 dark:hover:bg-lime-300 dark:text-zinc-900'
             >
                 Update Changes
             </Button>
