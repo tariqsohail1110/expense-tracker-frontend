e46c3d7150f9e20eb0c992595f8e8f583e304785
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-08-07 17:58:48 +0500

    feat: implement responsives for Admin page for mobile devices

--- src/components/Admin/AdminInfo.jsx
+++ src/components/Admin/AdminInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AdminInfo({text, number}) {
     return (
         <>
-            <div className='rounded-lg shadow-lg text-zinc-900 bg-white p-6 duration-500 dark:bg-zinc-700'>
+            <div className='rounded-lg shadow-lg text-zinc-900 bg-white p-6 duration-500 dark:bg-zinc-700 mt-6 lg:m-0'>
                 <h1 className='font-mono text-xs duration-500 dark:text-white'>{text}</h1>
                 <h1 className='text-3xl font-sans font-bold mt-4 duration-500 dark:text-white'>{number}</h1>
             </div>
