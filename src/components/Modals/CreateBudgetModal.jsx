bdcbd4c234b8989752f0681ee4f06534337a3b28
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-09-01 02:32:25 +0500

    feat: implement backend integration in create budget component

--- src/components/Modals/CreateBudgetModal.jsx
+++ src/components/Modals/CreateBudgetModal.jsx
@@ -1,7 +1,10 @@
 import React, { useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { X } from 'lucide-react';
-import { Button, Input, Dropdown } from '../index.js';
+import { Button, Input } from '../index.js';
+import { useForm } from 'react-hook-form';
+import { numberRegex } from '../../common/constants.js';
+import api from '../../config/axios.config.js';
 
 function CreateBudgetModal({ isOpen = true, onClose }) {
     if (!isOpen) return null;
@@ -14,9 +17,32 @@ function CreateBudgetModal({ isOpen = true, onClose }) {
         }
     };
 
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        if (!numberRegex.test(data.budget)) {
+            setError('budget', { message: 'Numbers only' });
+            return;
+        }
+        try {
+            await api.post('/api/v1/budget/', {
+                totalBudget: Number(data.budget)
+            });
+            onClose();
+            window.location.reload();
+        }catch(error) {
+            setError('budget', { message: error.message })
+        }
+    }
+
     return createPortal(
         <div ref={modalRef} onClick={closeModal} className='fixed inset-0 z-[100] bg-black/40       backdrop-blur-sm text-zinc-900 dark:text-white flex justify-center items-center p-4'>
-            <div className='rounded-lg shadow-xl text-zinc-900 bg-white p-6 dark:bg-zinc-700 dark:text-white duration-500 w-full max-w-xl space-y-4'>
+            <form onSubmit={handleSubmit(onSubmit)} className='rounded-lg shadow-xl text-zinc-900 bg-white p-6 dark:bg-zinc-700 dark:text-white duration-500 w-full max-w-xl space-y-4'>
                 <div className='flex justify-between items-center pb-2 border-b dark:border-zinc-600'>
                     <h1 className='font-bold text-lg font-sans'>
                         Create Budget
@@ -29,22 +55,24 @@ function CreateBudgetModal({ isOpen = true, onClose }) {
                     </Button>
                 </div>
                 
-                <Input 
+                <Input
+                    {...register('budget', { required: { value: true, message: 'Budget os required' }})}
                     label='amount' 
                     type='number' 
                     placeholder='50000' 
                     className='border-2 focus:border-black duration-500 dark:bg-zinc-700 dark:border-zinc-600 dark:focus:bg-zinc-700 dark:text-white dark:focus:border-zinc-800' 
                 />
+                {errors.budget && <p className="text-red-500 text-center text-xs mt-1 ml-1">{errors.budget.message}</p>}
                 
                 <Button
-                    onClick={() => onClose(false)}
+                    type='submit'
                     bgColor='bg-slate-900'
                     textColor='text-white'
                     className='w-full font-bold hover:bg-slate-800 duration-200 hover:duration-200 dark:bg-lime-600 dark:hover:bg-lime-500 dark:text-zinc-900 flex gap-1 justify-center items-center !mt-6'
                 >
                     Create
                 </Button>
-            </div>
+            </form>
         </div>,
         document.body
     );
