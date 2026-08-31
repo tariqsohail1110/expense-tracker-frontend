84a0445e26d2e994ffe8c633e797e8bec5b0bb0a
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-08-31 23:32:53 +0500

    feat: implement forward ref in dropdown component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import React, { useState, useId } from 'react'
 
-function Dropdown({label, categories, className = '', containerClassName = ''}) {
+function Dropdown({label, categories, className = '', containerClassName = '', ...props}, ref) {
+    const id = useId();
     const [selectedCat, setSelectedCat] = useState(categories[0] || '');
 
     const handleChange = (e) => {
@@ -10,22 +11,25 @@ function Dropdown({label, categories, className = '', containerClassName = ''}) 
     return (
         <div className={`w-full font-sans text-sm ${containerClassName}`}>
             {label && (
-                <label className='inline-block mb-1 font-medium capitalize text-zinc-700 dark:text-zinc-200'>
+                <label htmlFor={id} className='inline-block mb-1 font-medium capitalize text-zinc-700 dark:text-zinc-200'>
                     {label}
                 </label>
             )}
             <select 
+                id={id}
+                ref={ref}
                 className={`border-2 h-10 w-full px-2 rounded-lg focus:border-black duration-500 dark:bg-zinc-700 dark:border-zinc-600 dark:focus:bg-zinc-700 dark:text-white dark:focus:border-zinc-800 ${className}`} 
-                id="categories" 
+                {...props}
                 value={selectedCat} 
                 onChange={handleChange}
             >
                 {categories.map((cat, index) => {
-                    return <option key={cat || index} className='text-xs' value={cat}>{cat}</option>
+                    return <option key={cat || index} className='text-xs' value={cat}
+                    >{cat}</option>
                 })}
             </select>
         </div>
     )
 }
 
-export default Dropdown
+export default React.forwardRef(Dropdown);
