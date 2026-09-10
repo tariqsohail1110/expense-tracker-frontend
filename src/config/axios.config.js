2e3f9311f0748c41b4e839f41e6cfe024acc9631
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-09-10 22:05:25 +0500

    feat: modify baseurl in axios config

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://vault-finance-application.onrender.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
